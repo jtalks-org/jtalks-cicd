d5bd455304b8abb69cfc95a317c9c4c293f607d1
ctapobep <ctapobep@javatalks.ru>
2015-02-22 19:11:03 +0300

    JC-2138 Clean up containers & images after a run
    Next:
    - Change ApplicationContext to create respective DeploymentCommand,
     Tomcat, Nexus, JtalksArtifacts
    - Add plugins to installation process

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"
 docker build --no-cache=true -t jtalks/cicd-tests . || error='Error during image building'
-docker run jtalks/cicd-tests || error='Error during container run'
+docker run --rm jtalks/cicd-tests || error='Error during container run'
 docker rmi jtalks/cicd-tests
 if [ ! -z "$error" ]; then
   echo "$error"
