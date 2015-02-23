163cd0b149bc02195ebcbdf34be7cf3f5d1a6379
ctapobep <ctapobep@javatalks.ru>
2015-02-24 02:29:10 +0300

    JC-2138 Fix Config Grabber to create correct dir structure

--- jtalks/util/EnvironmentConfigGrabber.py
+++ jtalks/util/EnvironmentConfigGrabber.py
@@ -6,18 +6,19 @@ from jtalks.util.Logger import Logger
 
 
 class EnvironmentConfigGrabber:
-    logger = Logger("EnvironmentConfigGrabber")
+    logger = Logger('EnvironmentConfigGrabber')
 
     def __init__(self, env_configs_root, temp_dir):
         self.env_configs_root = env_configs_root
         self.temp_dir = temp_dir
         self.clone_repo_to = os.path.join(temp_dir, 'environments')
-        self.grabbed_configs_location = os.path.join(self.clone_repo_to, "configs")
+        self.grabbed_configs_location = os.path.join(self.clone_repo_to, 'configs')
 
     def grab_jtalks_configs(self):
         try:
             self.__remove_previous_git_folder__()
             self.__create_jtalks_temp_dir__()
+            self.logger.info('Cloning environments to {0}', self.clone_repo_to)
             repo.Repo.clone_from('git@jtalks.org:environments', self.clone_repo_to)
             self.__copy_grabbed_configs_into_work_dir__()
         except GitCommandError as e:
@@ -40,11 +41,11 @@ class EnvironmentConfigGrabber:
     def __copy_grabbed_configs_into_work_dir__(self):
         grabbed_dirs_and_files = os.listdir(self.grabbed_configs_location)
         for next_grabbed_file_or_dir in grabbed_dirs_and_files:
-            destination_file = self.env_configs_root + next_grabbed_file_or_dir
+            destination_file = os.path.join(self.env_configs_root, next_grabbed_file_or_dir)
+            self.logger.info('Creating dir [{0}]', os.path.abspath(destination_file))
             if os.path.exists(destination_file):
-                self.logger.info("{0} will be overwritten by newer version from git repo", destination_file)
                 self.__delete_file_or_dir__(destination_file)
-            shutil.move(self.grabbed_configs_location + "/" + next_grabbed_file_or_dir, destination_file)
+            shutil.move(os.path.join(self.grabbed_configs_location, next_grabbed_file_or_dir), destination_file)
 
     def __delete_file_or_dir__(self, destination_file):
         if os.path.isfile(destination_file):
